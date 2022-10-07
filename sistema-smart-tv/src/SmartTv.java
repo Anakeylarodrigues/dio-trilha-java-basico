8bd54de5538ac7bdcf0e0f82874e64bd591f8bf6
Anakeylarodrigues <anakeylarodrigues1@gmail.com>
2022-10-07 12:29:59 -0300

    implementando o projeto

--- sistema-smart-tv/src/SmartTv.java
+++ sistema-smart-tv/src/SmartTv.java
@@ -1,3 +1,24 @@
 public class SmartTv {
     
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 15;
+
+    public void ligar () {
+        ligada = true;
+    }
+
+    public void aumentarVolume() {
+        volume++;
+        System.out.println("Aumentando volume para: " + volume);
+    }
+
+    public void diminuirVolume() {
+        volume--;
+        System.out.println("Diminuindo volume para: " + volume);
+    }
+
+    public void desligar () {
+        ligada = false;
+    }
 }
--- sistema-smart-tv/src/Usuario.java
+++ sistema-smart-tv/src/Usuario.java
@@ -1,5 +1,23 @@
 public class Usuario {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("TV Ligada? " + smartTv.ligada);
+        System.out.println("Canal Atual: " + smartTv.canal);
+        System.out.println("Volume Atual: " + smartTv.volume);
+
+        smartTv.ligar();
+        System.out.println("Novo Status -> TV Ligada? " + smartTv.ligada);
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+        System.out.println("Volume Atual: " + smartTv.volume);
+
+        smartTv.desligar();
+        System.out.println("Novo Status -> TV Ligada? " + smartTv.ligada);
     }
 }
